4c618ee1d856a4bcae3323934633a929ce4a3ed7
baki <badrkhatib@hotmail.com>
2024-05-18 14:19:28 +0200

    need to fix some things - base path etc ..

--- encrypt-decrypt.py
+++ encrypt-decrypt.py
@@ -1,11 +1,15 @@
 import os
 from hashlib import sha256
+from cryptography.fernet import Fernet
 
-#directory PATH from where you execute the program
+#directory PATH of the dir containing files to encrypt/decrypt
 directory = '../toencrypt/'
 
-#key for the encryption/decryption
+#key for the encryption/decryption files
 key = 'test'
+
+fkey = Fernet.generate_key()
+fernet = Fernet(fkey)
 
 def encrypt(toencrypt: str, output: str, key:str):
     keys = sha256(key.encode('utf-8')).digest()
@@ -19,25 +23,31 @@ def encrypt(toencrypt: str, output: str, key:str):
                 f_output.write(b)
                 i += 1
 
-def decrypt(toencrypt: str, output: str, key:str):
+def decrypt(todecrypt: str, output: str, key:str):
     keys = sha256(key.encode('utf-8')).digest()
-    with open(toencrypt, 'rb') as f_toencrypt:
+    with open(todecrypt, 'rb') as f_todecrypt:
         with open(output, 'wb') as f_output:
             i = 0
-            while f_toencrypt.peek():
-                c = ord(f_toencrypt.read(1))
+            while f_todecrypt.peek():
+                c = ord(f_todecrypt.read(1))
                 j = i % len(keys)
                 b = bytes([c^keys[j]])
                 f_output.write(b)
                 i += 1
+
+def crypt_filename(filename: str) -> str:
+    encfilename = fernet.encrypt(filename.encode())
+    return(encfilename)
 
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     # checking if it is a file
     if os.path.isfile(f):
         ext = f[f.find(".", 4) + 1:]
+        base_path = f[:f.rfind('/')] + '/'
+        print(base_path)
         if ext == "crypt":
-            decrypt(f, f[:f.find(".", 4) + 1] + "txt", key)
+            decrypt(f, fernet.decrypt(fname).decode(), key)
         else:
-            encrypt(f, f[:f.find(".", 4) + 1] + "crypt", key)
+            encrypt(f, base_path + str(fernet.encrypt(filename.encode())) + ".crypt", key)
         os.remove(f)
